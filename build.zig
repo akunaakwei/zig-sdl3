e200b0ede86adafb358fb26e9b44ab1705a70934
Akuna Akwei <akunaakwei@gmail.com>
2026-08-29 08:07:04 +0200

    add x11 xdbe support

--- build.zig
+++ build.zig
@@ -173,7 +173,7 @@ pub fn build(b: *std.Build) void {
     const x11 = b.option(bool, "SDL_X11", "Use X11 video driver") orelse linux and video;
     // const x11_shared = b.option(bool, "SDL_X11_SHARED", "Dynamically load X11 support") orelse x11 and false;
     const x11_xcursor = b.option(bool, "SDL_X11_XCURSOR", "Enable Xcursor support") orelse x11;
-    const x11_xdbe = b.option(bool, "SDL_X11_XDBE", "Enable Xdbe support") orelse x11 and false;
+    const x11_xdbe = b.option(bool, "SDL_X11_XDBE", "Enable Xdbe support") orelse x11;
     const x11_xinput = b.option(bool, "SDL_X11_XINPUT", "Enable XInput support") orelse x11 and false;
     const x11_xfixes = b.option(bool, "SDL_X11_XFIXES", "Enable Xfixes support") orelse x11 and false;
     const x11_xrandr = b.option(bool, "SDL_X11_XRANDR", "Enable Xrandr support") orelse x11 and false;
